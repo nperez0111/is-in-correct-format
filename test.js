b3c0af7eb182387ee7d5e570681fdf4c0650e0d7
Nick The Sick <nperez0111@gmail.com>
2016-09-06 11:22:35 -0700

    add test cases for first working version

--- test.js
+++ test.js
@@ -1,6 +1,27 @@
 import test from 'ava';
 import fn from './';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
-});
+test( 'returns proper bool if each key is explicit in location', t => {
+    t.true( fn( {
+        a: 2
+    }, {
+        a: fn.is.number
+    } ) )
+    t.false( fn( {
+        a: 2
+    }, {
+        a: fn.is.function
+    } ) )
+} );
+
+test( 'deeply nested', t => {
+    t.true( fn( {
+        a: {
+            b: 2
+        }
+    }, {
+        a: {
+            b: fn.is.number
+        }
+    } ) )
+} )
