c7ebcd0aaaf9d58b38d1a932a5f3f78877c3e07f
Nick The Sick <nperez0111@gmail.com>
2016-09-06 13:55:41 -0700

    cleanup and dead code removal

--- index.js
+++ index.js
@@ -10,49 +10,46 @@ const is = {
         symbol: require( 'is-symbol' ),
         boolean: a => a === true || a === false
     },
-    extender = require( 'objextension' )
-let please = null
-    /*example usage
-    var isItInCorrectFormat= require('is-in-correct-format');
-    isItInCorrectFormat({a:2},{key:String,value:Number})
-    var is=IsItInCorrectformat.is;
-    isItInCorrectFormat({a:{b:3}},{a:{b:is.number}})
-    isItInCorrectFormat({a:{b:3}},{a:{b:a=>is.number(a)||is.function(a)}})
-    */
-module.exports = ( obj, check, allValues ) => {
-    let flag = false
-    if ( !obj || !check )
-        return false
-    if ( allValues === undefined ) {
-        allValues = a => true
-        flag = true
+    extender = require( 'objextension' ),
+    checkAllVals = function ( obj, checker ) {
+        return Object.keys( obj ).every( function ( key ) {
+            let val = obj[ key ]
+            if ( is.object( val ) ) {
+                return checkAllVals( val, checker )
+            }
+            let ret = checker( val )
+            return is.boolean( ret ) ? ret : typeof checker( val ) === typeof val
+        } )
+    },
+    traverseObj = function ( obj, checker ) {
+        return Object.keys( checker ).every( function ( key ) {
+            let val = obj[ key ],
+                check = checker[ key ]
+            if ( is.object( val ) ) {
+                return traverseObj( val, check )
+            } else {
+                if ( is.function( check ) ) {
+
+                    return check( val, key )
+                }
+                return val instanceof check
+            }
+        } )
     }
 
-    let checkAllVals = function ( obj, checker ) {
-            return Object.keys( obj ).every( function ( key ) {
-                let val = obj[ key ]
-                if ( is.object( val ) ) {
-                    return checkAllVals( val, checker )
-                }
-                let ret = checker( val )
-                return is.boolean( ret ) ? ret : typeof checker( val ) === typeof val
-            } )
-        },
-        traverseObj = function ( obj, checker ) {
-            return Object.keys( checker ).every( function ( key ) {
-                let val = obj[ key ],
-                    check = checker[ key ]
-                if ( is.object( val ) ) {
-                    return traverseObj( val, check )
-                } else {
-                    if ( is.function( check ) ) {
+let please = null
 
-                        return check( val, key )
-                    }
-                    return val instanceof check
-                }
-            } )
-        }
+module.exports = ( obj, check, allValues ) => {
+
+    if ( !obj || !check )
+        return false
+
+    if ( allValues === undefined ) {
+
+        allValues = a => true
+
+    }
+
     return traverseObj( obj, check ) && checkAllVals( obj, allValues )
 
 
